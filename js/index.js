43385c66e2e34921b5a706a39f96cc90bbc757f1
reikx <raylinn@me.com>
2018-11-05 19:59:29 +0900

    created stage class

--- js/index.js
+++ js/index.js
@@ -1,46 +1,11 @@
-class WireBox{
-    constructor(x,y,z,s,scene){
-        const linemat = new THREE.LineBasicMaterial({
-            color: 0x4081e8,
-            linewidth: 60
-        });
-        s /= 2;
-        this.hs = s;
-        this.lines = [];
-        for (let i = 0;i < 2;++i){
-            for (let j = 0;j < 2;++j){
-                const l1 = new THREE.Geometry();
-                l1.vertices.push(
-                    new THREE.Vector3(x, y + ((i % 2 === 0) ? s : -s), z + ((j % 2 === 0) ? s : -s)),
-                    new THREE.Vector3(x, y + ((i % 2 === 0) ? s : -s), z + ((j % 2 === 0) ? s : -s))
-                );
-                const l2 = new THREE.Geometry();
-                l2.vertices.push(
-                    new THREE.Vector3(x + ((i % 2 === 0) ? s : -s), y , z + ((j % 2 === 0) ? s : -s)),
-                    new THREE.Vector3(x + ((i % 2 === 0) ? s : -s), y , z + ((j % 2 === 0) ? s : -s))
-                );
-                const l3 = new THREE.Geometry();
-                l3.vertices.push(
-                    new THREE.Vector3(x + ((i % 2 === 0) ? s : -s), y + ((j % 2 === 0) ? s : -s), z),
-                    new THREE.Vector3(x + ((i % 2 === 0) ? s : -s), y + ((j % 2 === 0) ? s : -s), z),
-                );
-                l1.elementsNeedUpdate = true;
-                l2.elementsNeedUpdate = true;
-                l3.elementsNeedUpdate = true;
-                lines.push(l1,l2,l3);
-                scene.add(new THREE.Line(l1,linemat));
-                scene.add(new THREE.Line(l2,linemat));
-                scene.add(new THREE.Line(l3,linemat));
-            }
-        }
+class StageBase{
+    constructor(scene,camera){
+        this.scene = scene;
+        this.camera = camera;
     }
 
-    animation(per){
-        for (let i = 0;i < 2;++i) {
-            for (let j = 0; j < 2; ++j) {
+    update(count){
 
-            }
-        }
     }
 }
 
@@ -52,7 +17,6 @@ class WireBase{
         this.maxCount = max;
         this.maxLength = (max + 1) * (n * 2 + 1) * 3;
         this.nowLength = 0;
-        this.lastZ = 0;
 
         this.geometry = new THREE.BufferGeometry();
         this.geometry.addAttribute('position',new THREE.BufferAttribute(new Float32Array(this.maxLength * 3),3));
@@ -66,9 +30,6 @@ class WireBase{
             let x = i * size + Math.random() * (size / 2);
             let y = Math.random() * size;
             let z = this.count * size + Math.random() * (size / 2);
-           // this.geometry.vertices.push(new THREE.Vector3(x,y,z));
-
-           // console.log("Huh?x ->" + x);
             this.geometry.attributes.position.array[this.nowLength * 3] = x;
             this.geometry.attributes.position.array[this.nowLength * 3 + 1] = y;
             this.geometry.attributes.position.array[this.nowLength * 3 + 2] = z;
@@ -79,41 +40,26 @@ class WireBase{
         }
 
         this.geometry.addGroup(0, this.maxCount, 0);
-        // console.log(this.geometry.vertices);
         this.count = max;
         this.line = new THREE.Line(this.geometry,linemat);
         this.line.frustumCulled = false;
-     /*   this.box = new THREE.BoxHelper(new THREE.Line(this.geometry,linemat));
-        this.box.update();*/
         console.log(this.geometry.position);
         scene.add(this.line);
-      //  scene.add(this.box);
     }
 
     next(){
         ++this.count;
         this.geometry.attributes.position.needsUpdate = true;
-      //  console.log(this.geometry.attributes.position.needsUpdate);
-       // console.log(this.geometry.vertices);
         if(this.count > this.maxCount){
-        //    console.log(this.geometry.attributes.position.array[(this.nowLength - 1) * 3 + 2]);
-           // console.log(this.geometry.attributes.position.array);
             const del = this.n * 6 + 2;//(this.n * 2 + 1) * 2 - 1 + (this.n * 2 + 1);
-  //          console.log(del);
             const newArr = new Float32Array(this.maxLength * 3);
             newArr.set(this.geometry.attributes.position.array.subarray(del * 3));
 
 
             this.geometry.attributes.position.array = newArr;
-           /* this.line.box.min.z += this.size;
-            console.log(this.line.box.min.z);*/
             this.nowLength -= del;
-    //       console.log(this.geometry.attributes.position.array[(this.nowLength - 1) * 3 + 2]);
-            //console.log(this.nowLength);
         }
 
-      //  console.log(this.geometry.vertices);
-      //  console.log("f:" + this.geometry.verticesNeedUpdate + "/" + this.geometry.vertices.length);
 
         let arr = [];
         for (let i = this.n;i >= -this.n; --i) {
@@ -123,15 +69,11 @@ class WireBase{
             let y = Math.random() * this.size;
             let z = this.count * this.size + (Math.random() / 2) * this.size;
 
-          //  console.log(z);
             const v = new THREE.Vector3(x,y,z);
             arr.unshift(v);
 
             let c = this.nowLength - (this.n - i + 1) * 3 + 1;
             c *= 3;
-           // console.log(this.geometry.vertices.length + ":" + c);
-//          //  console.log(this.geometry.vertices[this.n + i].x, this.geometry.vertices[this.n + i].y, this.geometry.vertices[this.n + i].z);*/
-        //    console.log(v);
             if(i > -this.n){
                 let ox = array[c];
                 let oy = array[c + 1];
@@ -142,7 +84,6 @@ class WireBase{
                 array[this.nowLength * 3 + 3] = ox;
                 array[this.nowLength * 3 + 4] = oy;
                 array[this.nowLength * 3 + 5] = oz;
-             //   console.log(c + "," + i + "," + ox + "," + oy + "," + oz);
                 this.nowLength += 2;
             }
             else{
@@ -153,20 +94,25 @@ class WireBase{
             }
         }
         for(let i = -this.n;i <= this.n;++i){
-           // console.log(i + this.n);
             const array = this.geometry.attributes.position.array;
             array[this.nowLength * 3] = arr[i + this.n].x;
             array[this.nowLength * 3 + 1] = arr[i + this.n].y;
             array[this.nowLength * 3 + 2] = arr[i + this.n].z;
             this.nowLength += 1;
-       //     this.geometry.vertices.push(arr[i + this.n]);
-          //  console.log(arr[i + this.n]);
         }
-     //   console.log(this.geometry.attributes.position.needsUpdate);
-
-
         this.geometry.setDrawRange(0,this.nowLength - 1);
-   //     console.log("l:" + this.geometry.verticesNeedUpdate + "/" + this.geometry.vertices.length);
+    }
+}
+
+class WireBaseStage extends StageBase{
+    constructor(scene,camera){
+        super(scene,camera);
+        this.wireBase = new WireBase(100,30,50,this.scene);
+    }
+
+    update(count){
+        if((count % 10) === 0)this.wireBase.next();
+        this.camera.position.z += 10;
     }
 }
 
@@ -189,7 +135,6 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
     const composer = new THREE.EffectComposer(renderer);
 
     const camera = new THREE.PerspectiveCamera(45, width / height,10,100000);
-   // camera.lookAt(new THREE.Vector3(0,0,1000));
     camera.position.set(0, 300, 0);
     camera.lookAt(0,300,10);
 
@@ -220,37 +165,39 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
         vertexShader:ppv,
         fragmentShader:ppf
     };
+
+
+    let textGeo = new THREE.TextGeometry(baseLogo,{
+        font:font,
+        size:75,
+        height:1,
+        curveSegments: 12
+    });
+
+    textGeo.center();
+    const text = new THREE.LineSegments(new THREE.EdgesGeometry(textGeo),new THREE.LineBasicMaterial({
+        color: 0xaaaaaa
+    }));
+    text.position.z = -2000;
+    camera.add(text);
+    scene.add(camera);
+
     const shaderPass = new THREE.ShaderPass(THREE.PostProcessShader);
     shaderPass.renderToScreen = true;
     composer.addPass(renderPass);
     composer.addPass(shaderPass);
     console.log(shaderPass);
 
+   // const wb = new WireBase(100,30,50,scene);
+    const wbStage = new WireBaseStage(scene,camera);
 
-    const wb = new WireBase(100,30,50,scene);
-
-    let textGeo = new THREE.TextGeometry(baseLogo,{
-        font:font,
-        size:100,
-        height:1,
-        curveSegments: 12
-    });
-
-    textGeo.center();
-    let text = new THREE.LineSegments(new THREE.EdgesGeometry(textGeo),new THREE.LineBasicMaterial({
-        color: 0xaaaaaa
-    }));
-    text.rotation.y = Math.PI;
-    text.position.y = 320;
-    text.position.z = 2000;
-    scene.add(text);
-  //  scene.add(new THREE.AxisHelper(20000));
     let randLogoArr = Array(baseLogo.length);
     let nowLogo = baseLogo;
     let count = 0;
     const update = () => {
-        if((count % 10) === 0)wb.next();
+       // if((count % 10) === 0)wb.next();
       //  wb.moveBack(50);
+        wbStage.update(count);
         for (let i = 0;i < 8;++i){
             if(i === 5)continue;
             if(randLogoArr[i] > 0){
@@ -277,14 +224,13 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
         }
         text.geometry = new THREE.EdgesGeometry(new THREE.TextGeometry(nowLogo,{
             font:font,
-            size:100,
+            size:75,
             height:1,
             curveSegments: 12
         }));
         text.geometry.center();
       //  camera.rotation.y += 0.01;
-        camera.position.z += 10;
-        text.position.z += 10;
+    //    camera.position.z += 10;
        // renderer.render(scene, camera);
        // THREE.PostProcessShader.uniforms.uTime.value = Math.random();
           ++shaderPass.material.uniforms.u_count.value;
@@ -295,10 +241,8 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
         ++count;
     };
     update();
-
-
 }));
 
-function replaceRandom() {
+function createCamera() {
 
 }
--- shader/fragment.frag
+++ shader/fragment.frag
@@ -18,6 +18,7 @@ void main(void){
   // gl_FragColor = vec4(1,1,uTime,1);
    float r1 = random(vec2(int(gl_FragCoord.y)/ 5,u_count / 5));
    float r2 = random(vec2(int(gl_FragCoord.y)/ 10,u_count));
+
    vec2 newPos = (r1 < 0.2) && (mod(u_count,100) < 20) ? vec2(vUv.x + (mod(u_count,2) == 0 ? 0.05 : -0.05),vUv.y) : vec2(vUv.x,vUv.y);
    float a = (r2 < 0.5) ? random(vec2(gl_FragCoord.x + float(u_count) * 0.02,gl_FragCoord.y)) * 0.05 : 0.0;
    vec4 color = vec4(texture2D(tDiffuse,vec2(newPos.x + 0.002,newPos.y)).r + a,texture2D(tDiffuse,vec2(newPos.x + 0.004,newPos.y)).g + a,texture2D(tDiffuse,vec2(newPos.x + 0.006,newPos.y)).b + a,1.0);
