bb25b5eb0e84666f7f207981af2f1b2db816f858
reikx <raylinn@me.com>
2018-11-09 21:47:03 +0900

    added logo animations

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
-
-
 class WireGround{
     constructor(size,n,max,scene){
         this.count = 0;
-        this.size = size;
-        this.n = n;
+        this.size = size;//grid span
+        this.n = n;//grid number
         this.maxCount = max;
         this.maxLength = (max + 1) * (n * 2 + 1) * 3;
         this.nowLength = 0;
@@ -13,7 +11,7 @@ class WireGround{
         this.geometry.addAttribute('position',new THREE.BufferAttribute(new Float32Array(this.maxLength * 3),3));
 
         const linemat = new THREE.LineBasicMaterial({
-            color: 0x333333,
+            color: 0x333333
         });
         for (let i = -n;i <= n;++i){
             let x = i * size + Math.random() * (size / 2);
@@ -92,6 +90,67 @@ class WireGround{
     }
 }
 
+class WireCubesStage extends StageBase{
+    constructor(scene,camera) {
+        super(scene, camera);
+        this.lastPos = new THREE.Vector3(0,0,0);
+        this.nowPos = new THREE.Vector3(0,0,0);
+        this.size = 100;
+        this.max = 50;
+        this.cubeArr = [new THREE.Mesh(new THREE.CubeGeometry(this.size,this.size,this.size),new THREE.MeshBasicMaterial({color:0xffaa33}))];
+        scene.add(this.cubeArr[0]);
+        camera.position.x = 300;
+        camera.position.y = 300;
+        camera.position.z = 300;
+        camera.lookAt(this.lastPos);
+    }
+
+    update(count){
+        const t = 2.0;
+        const k = count % t;
+        const lookAt = new THREE.Vector3(this.lastPos.x + (this.nowPos.x - this.lastPos.x) / t * k,this.lastPos.y + (this.nowPos.y - this.lastPos.y) / t * k,this.lastPos.z + (this.nowPos.z - this.lastPos.z) / t * k);
+        const d1 = Math.PI * 2 * ((count % 240) / 240.0);
+        const d2 = Math.PI * 2 * ((count % 720) / 720.0);
+
+        // console.log(lookAt);
+        this.camera.position.x = lookAt.x + Math.cos(d2) * 500;
+        this.camera.position.z = lookAt.z + Math.sin(d2) * 2500;
+        this.camera.position.y = lookAt.y + Math.sin(d2) * 3000;
+        this.camera.lookAt(lookAt);
+        if(count % t === 0){
+            this.lastPos = this.nowPos;
+            const s = parseInt(Math.random() * 3);
+            if(s === 0){
+                this.nowPos = new THREE.Vector3(this.lastPos.x + this.size,this.lastPos.y,this.lastPos.z);
+            }
+            else if(s === 1){
+                this.nowPos = new THREE.Vector3(this.lastPos.x,this.lastPos.y + this.size,this.lastPos.z);
+            }
+            else if(s === 2){
+                this.nowPos = new THREE.Vector3(this.lastPos.x,this.lastPos.y,this.lastPos.z + this.size);
+            }
+            const cube = new THREE.Mesh(new THREE.CubeGeometry(this.size,this.size,this.size),new THREE.MeshBasicMaterial({color:0x888888}));
+            cube.position.x = this.nowPos.x;
+            cube.position.y = this.nowPos.y;
+            cube.position.z = this.nowPos.z;
+           // console.log(cube.position.x,cube.position.y,cube.position.z);
+            this.cubeArr.push(cube);
+            this.scene.add(cube);
+            if(this.cubeArr.length >= this.max){
+                this.scene.remove(this.cubeArr[0]);
+                this.cubeArr.shift();
+            }
+            for (let i = 0;i < this.cubeArr.length - 1;++i){
+
+             //   console.log(this.cubeArr[this.cubeArr.length - i - 1].material.color);
+                let cc = 1 - i * (1.0 / this.max);
+                this.cubeArr[i].material.color = new THREE.Color(cc,cc,cc);
+            }
+        }
+
+    }
+}
+
 class WireBaseStage extends StageBase{
     constructor(scene,camera){
         super(scene,camera);
@@ -99,14 +158,94 @@ class WireBaseStage extends StageBase{
     }
 
     update(count){
-        if((count % 10) === 0)this.wireBase.next();
-        this.camera.position.z += 10;
+        if((count % 20) === 0)this.wireBase.next();
+        this.camera.position.z += 5;
     }
 }
 
+class RandomString{
+    constructor(base,now,rate,initRate,...fixed){
+        this.base = base;
+        this.now = now;
+        this.rate = rate;
+        this.initRate = initRate;
+        this.isInitialized = base === now;
+        this.fixed = fixed;
+        this.randList = new Array(base.length);
+    }
+
+    update(){
+        if(this.isInitialized) {
+            for (let i = 0; i < this.base.length; ++i) {
+                if (this.fixed.indexOf(i) >= 0) continue;
+                const c = this.randList[i] === 1 ? this.base.charCodeAt(i) : (32 + parseInt(Math.random() * 90));
+                if (this.randList[i] >= 1) {
+                    --this.randList[i];
+                    this.now = this.now.substr(0, i) + String.fromCharCode(c) + this.now.substr(i + 1, this.now.length);
+                }
+                else if (Math.random() <= this.rate) {
+                    this.randList[i] = 20;
+                }
+            }
+        }
+        else{
+            const s = this.base.length > this.now.length ? this.base.length : this.now.length;
+            for (let i = 0; i < s; ++i){
+                if(this.base.length < this.now.length && Math.random() <= this.initRate){
+                    this.now = this.now.substr(0, i) + this.now.substr(i + 1, this.now.length);
+                }
+                if (this.now.charCodeAt(i) === this.base.charCodeAt(i))continue;
+                const c = this.randList[i] === 1 ? this.base.charCodeAt(i) : (32 + parseInt(Math.random() * 90));
+                if (this.randList[i] >= 1) {
+                    --this.randList[i];
+                    this.now = this.now.substr(0, i) + String.fromCharCode(c) + this.now.substr(i + 1, this.now.length);
+                }
+                else if (Math.random() <= this.initRate) {
+                    this.randList[i] = 20;
+                }
+            }
+            if(this.now === this.base)this.isInitialized = true;
+        }
+        return this.now;
+    }
+}
+
+class Logo{
+    constructor(font){
+        this.rtext = new RandomString("devne.co","???????",0.005,0.5,5);
+        this.font = font;
+        this.textGeo = new THREE.TextGeometry(this.rtext.now,{
+            font:font,
+            size:0.5,
+            height:0.01,
+            curveSegments: 12
+        });
+        this.textMesh = new THREE.LineSegments(new THREE.EdgesGeometry(this.textGeo),new THREE.LineBasicMaterial({
+            color: 0x777777
+        }));
+        this.textMesh.geometry.center();
+        this.textMesh.position.z = -10;
+    }
+
+    update(){
+        this.textMesh.geometry = new THREE.EdgesGeometry(new THREE.TextGeometry(this.rtext.update(),{
+            font:this.font,
+            size:0.5,
+            height:0.01,
+            curveSegments: 1
+        }));
+        this.textMesh.geometry.center();
+    }
+
+    updateText(text,...fixed){
+        this.rtext = new RandomString(text,this.rtext.now,this.rtext.rate,this.rtext.initRate,fixed);
+    }
+}
+
+
+
 const fontLoader = new THREE.FontLoader();
 
-const baseLogo = "devne.co";
 const width = window.innerWidth;
 const height = window.innerHeight;
 
@@ -119,10 +258,9 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
     });
 
     renderer.setSize(width, height);
-
     const composer = new THREE.EffectComposer(renderer);
 
-    const camera = new THREE.PerspectiveCamera(45, width / height,10,100000);
+    const camera = new THREE.PerspectiveCamera(45, width / height,5,100000);
     camera.position.set(0, 300, 0);
     camera.lookAt(0,300,10);
 
@@ -135,11 +273,13 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
         url:'./shader/vertex.vert',
         async:false
     }).responseText;
+
     const ppf = $.ajax({
         type:'GET',
         url:'./shader/fragment.frag',
         async:false
     }).responseText;
+
     THREE.PostProcessShader = {
         uniforms:{
             "u_count":{
@@ -153,19 +293,38 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
         fragmentShader:ppf
     };
 
-    let textGeo = new THREE.TextGeometry(baseLogo,{
-        font:font,
-        size:75,
-        height:1,
-        curveSegments: 12
-    });
-
-    textGeo.center();
-    const text = new THREE.LineSegments(new THREE.EdgesGeometry(textGeo),new THREE.LineBasicMaterial({
-        color: 0xaaaaaa
+    const selector = new THREE.Geometry();
+    selector.vertices.push(
+        new THREE.Vector3(0,0,0),
+        new THREE.Vector3(1,1,0),
+        new THREE.Vector3(1 + 0.1,1 - 0.1,0),
+        new THREE.Vector3(0.1 * Math.sqrt(2),0,0),
+        new THREE.Vector3(1 + 0.1,-1 + 0.1,0),
+        new THREE.Vector3(1,-1,0),
+        new THREE.Vector3(0,0,0),
+        );
+    const selectorL = new THREE.Line(selector,new THREE.LineBasicMaterial({
+        color: 0x777777
     }));
-    text.position.z = -2000;
-    camera.add(text);
+    const selectorR = new THREE.Line(selector,new THREE.LineBasicMaterial({
+        color: 0x777777
+    }));
+    selectorR.rotation.y = Math.PI;
+    selectorL.position.z = -10;
+    selectorL.position.x = -width * 0.0057;
+    selectorL.scale.set(0.2,0.2,0.2);
+    selectorR.position.z = -10;
+    selectorR.position.x = width * 0.0057;
+    selectorR.scale.set(0.2,0.2,0.2);
+    camera.add(selectorL);
+    camera.add(selectorR);
+
+    const wbStage = new WireBaseStage(scene,camera);
+
+
+    const logo = new Logo(font);
+    camera.add(logo.textMesh);
+
     scene.add(camera);
 
     const shaderPass = new THREE.ShaderPass(THREE.PostProcessShader);
@@ -174,46 +333,14 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
     composer.addPass(shaderPass);
     console.log(shaderPass);
 
-    const wbStage = new WireBaseStage(scene,camera);
-
-    let randLogoArr = Array(baseLogo.length);
-    let nowLogo = baseLogo;
     let count = 0;
     const update = () => {
-       // if((count % 10) === 0)wb.next();
-      //  wb.moveBack(50);
-        wbStage.update(count);
-        for (let i = 0;i < 8;++i){
-            if(i === 5)continue;
-            if(randLogoArr[i] > 0){
-                let c = nowLogo.charCodeAt(i) + parseInt(Math.random() * 90);
-                if(c > 'z'.charCodeAt(0)){
-                   c -= 90;
-                }
-                --randLogoArr[i];
-                if(randLogoArr[i] === 0)nowLogo = nowLogo.substr(0,i) + baseLogo[i] + nowLogo.substr(i + 1,nowLogo.length);
-                else nowLogo = nowLogo.substr(0,i) + String.fromCharCode(c) + nowLogo.substr(i + 1,nowLogo.length);
-               // console.log(nowLogo);
-            }
-            else if(Math.random() < 0.0025){
-                randLogoArr[i] = 20;
-                let c = nowLogo.charCodeAt(i) + parseInt(Math.random() * 90);
-                if(c > 'z'.charCodeAt(0)){
-                    c -= 90;
-                }
-                --randLogoArr[i];
-                nowLogo = nowLogo.substr(0,i) + String.fromCharCode(c) + nowLogo.substr(i + 1,nowLogo.length);
-             //   nowLogo = nowLogo.split('').splice(i,1,String.fromCharCode(c)).join()
-              //  console.log(nowLogo);
-            }
-        }
-        text.geometry = new THREE.EdgesGeometry(new THREE.TextGeometry(nowLogo,{
-            font:font,
-            size:75,
-            height:1,
-            curveSegments: 12
-        }));
-        text.geometry.center();
+         wbStage.update(count);
+          if(count % 300 ===  0)logo.updateText("XcegsNmEF¥CjEFmf");
+          if(count % 300 ===  30)logo.updateText("devne.co");
+          if(count % 300 ===  150)logo.updateText("XcegsNmEF¥CjEFmf");
+          if(count % 300 ===  180)logo.updateText("Member");
+        logo.update();
       //  camera.rotation.y += 0.01;
     //    camera.position.z += 10;
        // renderer.render(scene, camera);
@@ -221,6 +348,7 @@ window.addEventListener('load', fontLoader.load('./font/technoid_one.json',(font
           ++shaderPass.material.uniforms.u_count.value;
      //   console.log(THREE.PostProcessShader.uniforms.uTime);
 
+       // composer.renderer.clear();
         composer.render();
         requestAnimationFrame(update);
         ++count;
--- shader/fragment.frag
+++ shader/fragment.frag
@@ -21,7 +21,7 @@ void main(void){
 
    vec2 newPos = (r1 < 0.2) && (mod(u_count,100) < 20) ? vec2(vUv.x + (mod(u_count,2) == 0 ? 0.05 : -0.05),vUv.y) : vec2(vUv.x,vUv.y);
    float a = (r2 < 0.5) ? random(vec2(gl_FragCoord.x + float(u_count) * 0.02,gl_FragCoord.y)) * 0.05 : 0.0;
-   vec4 color = vec4(texture2D(tDiffuse,vec2(newPos.x + 0.002,newPos.y)).r + a,texture2D(tDiffuse,vec2(newPos.x + 0.004,newPos.y)).g + a,texture2D(tDiffuse,vec2(newPos.x + 0.006,newPos.y)).b + a,1.0);
+   vec4 color = vec4(texture2D(tDiffuse,vec2(newPos.x + 0.002,newPos.y)).r + a,texture2D(tDiffuse,vec2(newPos.x + 0.004,newPos.y)).g + a,texture2D(tDiffuse,vec2(newPos.x + 0.006,newPos.y)).b + a,texture2D(tDiffuse,vec2(newPos.x,newPos.y)).a);
    color = u_count < 60 ? color * (float(u_count) / 60.0) : color;
    color *= 1.2;
    gl_FragColor = color;
